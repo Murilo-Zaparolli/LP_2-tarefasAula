a3bbb83f1d7197b72b0d3ee6bd8f386a783be638
murilo <20251cas0020002@estudantes.ifpr.edu.br>
2026-02-26 10:00:23 -0300

    semifinal update

--- aula2/src/AtividadesRoberta.java
+++ aula2/src/AtividadesRoberta.java
@@ -19,10 +19,10 @@ public class AtividadesRoberta {
     }
 
     static void exercicio2_somaDosElementosDaDiagonalPrincipal(int[][] matriz) {
-        // if (matriz.length != 6 || matriz[0].length != 6) {
-        //     System.out.println("matriz invalida, recomeçe o codigo para tentar de novo");
-        //     return;
-        // }
+        if (matriz.length != 6 || matriz[0].length != 6) {
+            System.out.println("matriz invalida, recomeçe o codigo para tentar de novo");
+            return;
+        }
 
         int soma = 0;
 
@@ -32,12 +32,6 @@ public class AtividadesRoberta {
                     soma += matriz[i][j];
                 }
             }
-        }
-        int aux;
-        for (int i = 0; i < matriz.length; i++) {
-            aux = matriz[i][i];
-            matriz[i][i] = matriz[i][matriz.length - 1 - i];
-            matriz[i][matriz.length - 1 - i] = aux;
         }
 
         System.out.printf("Exercicio 2: soma dos elementos da diagonal principal - resultado : %d\n", soma);
@@ -130,14 +124,15 @@ public class AtividadesRoberta {
 
         System.out.printf("Exercicio 6: matriz formada de elementos divididos pelo maior elemento da diagonal secundaria de outra matriz - resultado :\n");
 
-        for (int i = 0; i < matrizB.length; i++) {
-            for (int j = 0; j < matrizB.length; j++) {
-                System.out.printf("[%d][%d] -> [%.1f]\n", i, j, matrizB[i][j]);
-            }
-        }
+        chave.imprimirMatriz(matriz);
     }
 
     static void exercicio7_ProdutoMatricial(int[][] matrizA, int[][] matrizB) {
+        if (matrizA.length != 4 || matrizA[0].length != 6 && matrizB.length != 6 || matrizB[0].length != 4) {
+            System.out.println("matriz(es) invalida(s), recomeçe o codigo para tentar de novo");
+            return;
+        }
+
         int[][] matrizC = new int[matrizA.length][matrizB[0].length];
 
         for (int i = 0; i < matrizC.length; i++) {
@@ -150,14 +145,15 @@ public class AtividadesRoberta {
 
         System.out.printf("Exercicio 7: matriz formada de uma multiplicação de matrizes - resultado :\n");
 
-        for (int i = 0; i < matrizC.length; i++) {
-            for (int j = 0; j < matrizC.length; j++) {
-                System.out.printf("[%d][%d] -> [%d]\n", i, j, matrizC[i][j]);
-            }
-        }
+        chave.imprimirMatriz(matrizC);
     }
 
     static void exercicio8_MatrizMultiplicadaPorUmValorReal(int[][] matriz, int valor) {
+        if (matriz.length != 6 || matriz[0].length != 6) {
+            System.out.println("matriz invalida, recomeçe o codigo para tentar de novo");
+            return;
+        }
+
         int[] vetor = new int[matriz.length*matriz[0].length];
         int index = 0;
 
@@ -175,6 +171,11 @@ public class AtividadesRoberta {
     }
 
     static void exercicio9_TrocasEmUmaMatriz(int[][] matriz) {
+        if (matriz.length != 10 || matriz[0].length != 10) {
+            System.out.println("matriz invalida, recomeçe o codigo para tentar de novo");
+            return;
+        }
+
         int aux;
 
         for (int i = 0; i < matriz.length; i++) {
@@ -203,19 +204,154 @@ public class AtividadesRoberta {
 
         System.out.printf("Exercicio 9: troca de posições de uma matriz - resultado :\n");
 
+        chave.imprimirMatriz(matriz);
+    }
+
+    static void exercicio10_SomaDosElementosDasLinhasParesDeUmaMatriz(int[][] matriz) {
+        if (matriz.length != 9 || matriz[0].length != 9) {
+            System.out.println("matriz invalida, recomeçe o codigo para tentar de novo");
+            return;
+        }
+
+        int soma = 0;
+
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz.length; j++) {
-                System.out.printf("[%d][%d] -> [%d]\n", i, j, matriz[i][j]);
+                if (i%2 == 1) {
+                    break;
+                }
+
+                soma += matriz[i][j];
             }
         }
+
+        System.out.printf("Exercicio 10: soma dos elementos das linhas pares de uma matriz - resultado : %d\n", soma);
+    }
+
+    static void exercicio11_MultiplicaçãoDasLinhasDeUmaMatrizPeloDaDiagonalPrincipalDaLinha(int matriz[][]) {
+        if (matriz.length != 6 || matriz[0].length != 6) {
+            System.out.println("matriz invalida, recomeçe o codigo para tentar de novo");
+            return;
+        }
+
+        int valorDiagonalL;
+
+        for (int i = 0; i < matriz.length; i++) {
+            valorDiagonalL = matriz[i][i];
+
+            for (int j = 0; j < matriz[0].length; j++) {
+                matriz[i][j] *= valorDiagonalL;
+            }
+        }
+
+        System.out.printf("Exercicio 11: multiplicação das linhas de uma matriz pelo valor da diagonal principal da linha - resultado :\n");
+        chave.imprimirMatriz(matriz);
+    }
+
+    static void exercicio12_MediaAritmeticaDosElementosAbaixoDaDiagonalPrincipalDeUmaMatriz(int[][] matriz) {
+        if (matriz.length != 12 || matriz[0].length != 12) {
+            System.out.println("matriz invalida, recomeçe o codigo para tentar de novo");
+            return;
+        }
+
+        double resultado = 0;
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                if (j < i) {
+                    resultado += matriz[i][j];
+                }
+            }
+        }
+
+        resultado /= (double) (matriz.length * matriz[0].length);
+
+        System.out.printf("Exercicio 12: média aritmética dos elementos abaixo da diagonal principal de uma matriz - resultado: %.1f\n", resultado);
+    }
+
+    static void exercicio13_SomaDosElementosAcimaDaDiagonalPrincipalDeUmaMatriz(int[][] matriz) {
+        if (matriz.length != 10 || matriz[0].length != 10) {
+            System.out.println("matriz invalida, recomeçe o codigo para tentar de novo");
+            return;
+        }
+
+        int soma = 0;
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                if (j > i) {
+                    soma += matriz[i][j];
+                }
+            }
+        }
+
+        System.out.printf("Exercicio 13: soma dos elementos acima da diagonal principal de uma matriz - resultado: %d\n", soma);
     }
 
     public static void main(String[] args) {
-        int[][] matriz = new int[10][10];
+        System.out.println("Olá, para selecionar o exercicio solicitado, digite o numero do exercicio, eles são:\n");
+        System.out.println("Exercicio 1: soma dos elementos da matriz;");
+        System.out.println("Exercicio 2: soma dos elementos da diagonal principal;");
+        System.out.println("Exercicio 3: soma dos elementos da diagonal secundaria;");
+        System.out.println("Exercicio 4: soma dos elementos da linha 5 e coluna 3;");
+        System.out.println("Exercicio 5: menor elemento da diagonal secundaria;");
+        System.out.println("Exercicio 6: matriz formada de elementos divididos pelo maior elemento da diagonal secundaria de outra matriz;");
+        System.out.println("Exercicio 7: matriz formada de uma multiplicação de matrizes;");
+        System.out.println("Exercicio 8: vetor formado por uma matriz multiplicada por um valor real;");
+        System.out.println("Exercicio 9: troca de posições de uma matriz;");
+        System.out.println("Exercicio 10: soma dos elementos das linhas pares de uma matriz;");
+        System.out.println("Exercicio 11: multiplicação das linhas de uma matriz pelo valor da diagonal principal da linha;");
+        System.out.println("Exercicio 12: média aritmética dos elementos abaixo da diagonal principal de uma matriz;");
+        System.out.println("Exercicio 13: soma dos elementos acima da diagonal principal de uma matriz;");
 
-        chave.lerMatriz(matriz);
+        int exercicio;
 
-        exercicio2_somaDosElementosDaDiagonalPrincipal(matriz);
+        do {
+            exercicio = chave.SCAN.nextInt();
+
+            if (exercicio < 1 || exercicio > 13) {
+                System.out.println("Esse exercicio não existe. Por favor, selecione um dos 13 exercicios mostrados anteriormente.");
+                continue;
+            }
+
+            break;
+        } while (true);
+
+        switch (exercicio) {
+            case 1:
+                System.out.println("Exercicio 1: soma dos elementos da matriz - requerimentos:");
+                System.out.println("Uma matriz com ordem 5x5");
+                
+                int[][] matrizEX1 = new int[5][5];
+                chave.lerMatriz(matrizEX1);
+
+                exercicio1_somaDosElementosDaMatriz(matrizEX1);
+                break;
+        
+            case 2:
+                System.out.println("Exercicio 2: soma dos elementos da diagonal principal - requerimentos:");
+                System.out.println("Uma matriz com ordem 6x6");
+
+                int[][] matrizEX2 = new int[6][6];
+                chave.lerMatriz(matrizEX2);
+                break;
+
+            case 3:
+        }
     }
 }
 
+/*
+2 2 2 2 2 2 2 2 2 2 2 2
+2 2 2 2 2 2 2 2 2 2 2 5
+2 2 2 2 2 2 2 2 2 2 5 5
+2 2 2 2 2 2 2 2 2 5 5 5
+2 2 2 2 2 2 2 2 5 5 5 5
+2 2 2 2 2 2 2 5 5 5 5 5
+2 2 2 2 2 2 5 5 5 5 5 5
+2 2 2 2 2 5 5 5 5 5 5 5
+2 2 2 2 5 5 5 5 5 5 5 5
+2 2 2 5 5 5 5 5 5 5 5 5
+2 2 5 5 5 5 5 5 5 5 5 5
+2 5 5 5 5 5 5 5 5 5 5 5
+*/
