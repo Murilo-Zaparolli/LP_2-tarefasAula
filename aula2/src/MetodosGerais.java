913972c689c98920c1b6da124a190ff2baeccf1d
Murilo-Zaparolli <20251cas0020002@estudantes.ifpr.edu.br>
2026-02-25 23:19:15 -0300

    Update MetodosGerais.java

--- aula2/src/MetodosGerais.java
+++ aula2/src/MetodosGerais.java
@@ -3,12 +3,6 @@ import java.util.Scanner;
 public class MetodosGerais {
     final static Scanner SCAN = new Scanner(System.in);
 
-    
-
-    MetodosGerais(int man) {
-
-    }
-
     void imprimirMatriz(int[][] matriz) {
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[0].length; j++) {
@@ -18,10 +12,44 @@ public class MetodosGerais {
         }
     }
 
+    void imprimirMatriz(float[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.printf("[%d][%d] -> %.1f\n", i, j, matriz[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    void imprimirMatriz(double[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.printf("[%d][%d] -> %.1f\n", i, j, matriz[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
     void lerMatriz(int[][] matriz) {
         for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz.length; j++) {
+            for (int j = 0; j < matriz[0].length; j++) {
                 matriz[i][j] = SCAN.nextInt();
+            }
+        }
+    }
+
+    void lerMatriz(float[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                matriz[i][j] = SCAN.nextFloat();
+            }
+        }
+    }
+
+    void lerMatriz(double[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                matriz[i][j] = SCAN.nextDouble();
             }
         }
     }
